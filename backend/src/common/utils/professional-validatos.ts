a1efefd3848ba735376a45b9d01643a248479607
jummanso <julianamaciel97@gmail.com>
2025-11-12 23:46:30 -0300

    Remoção obrigatoriedade CRM e Coren

--- backend/src/common/utils/professional-validatos.ts
+++ backend/src/common/utils/professional-validatos.ts
@@ -1,6 +1,11 @@
 export const PROFESSIONAL_REGEX = {
+  // 🔹 NÃO MEXER
   CRM: /^CRM-[A-Z]{2}\s\d{4,7}$/,
   COREN: /^COREN-[A-Z]{2}\s\d{4,7}$/,
-  CRP: /^CRP\s\d{4,7}$/,
-  CREFITO: /^CREFITO\s\d{4,7}$/,
+
+  // 🔹 CRP – ex.: CRP 06/12345 ou CRP 06/12345-6
+  CRP: /^CRP\s?\d{2}\/\d{4,7}(-\d)?$/,
+
+  // 🔹 CREFITO – ex.: CREFITO-3 123456-F, CREFITO-3/123456-F, CREFITO 3 123456-F
+  CREFITO: /^CREFITO-?\d{1,2}[-\/ ]\d{4,7}-[A-Z]{1,3}$/,
 };
