ea3d21fdf11379d17b9634d5ebe531c541205cff
abnercosta97 <abner-rodrigo@hotmail.com>
2025-11-14 07:37:44 -0300

    corrigindo importação

--- backend/src/appointment/appointment.controller.ts
+++ backend/src/appointment/appointment.controller.ts
@@ -8,10 +8,13 @@ import {
   BadRequestException,
   Patch,
   Param,
+  Req,
 } from '@nestjs/common';
 import { AppointmentService } from './appointment.service';
 import { CreateAppointmentDto } from './dto/create-book.dto';
 import { UpdateAppointmentStatusDto } from './dto/update-appointment-status.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from 'src/common/enums/role.enum';
 
 @Controller('appointments')
 export class AppointmentController {
@@ -50,7 +53,8 @@ export class AppointmentController {
     @Param('id') id: string,
     @Body() dto: UpdateAppointmentStatusDto,
     @Req() req: any,
-) {
-  const userId = req.user.id; // vem do JWT
-  return this.appointmentService.updateStatusAsCaregiver(id, userId, dto.status);
+  ) {
+    const userId = req.user.id; // vem do JWT
+    return this.appointmentService.updateStatus(id, userId, dto.status);
+  }
 }
